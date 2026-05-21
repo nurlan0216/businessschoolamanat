1ad9df634d41f03d4ab5487963b2032e461765df
nurlan0216 <bolashaqsauda@gmail.com>
2026-05-21 15:36:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -811,7 +811,7 @@ function loadYtIframe(ytId, startSec) {
   const iframe = document.createElement('iframe');
   iframe.src = `https://www.youtube.com/embed/${ytId}?autoplay=1&start=${Math.max(0, Math.round(startSec))}&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1`;
   iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
-  iframe.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;border:none;pointer-events:none';
+  iframe.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;border:none;pointer-events:auto';
   slot.appendChild(iframe);
 }
 const getElapsedSec = () => Math.round((Date.now() - ytStartTime) / 1000);
